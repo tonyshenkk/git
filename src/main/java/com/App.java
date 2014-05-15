7c950c01f9d66b9ca3768aa178087aa23724ae15
“tonyshenkk” <906265000@qq.com>
2014-05-15 10:44:39 +0800

    Changes not staged for commit: 	modified:   src/.DS_Store
     Untracked files:
    	.DS_Store

---
+++ src/main/java/com/App.java
@@ -0,0 +1,20 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        p();
+
+    }
+
+    public static void p()
+    {
+        System.out.println("p");
+    }
+}
